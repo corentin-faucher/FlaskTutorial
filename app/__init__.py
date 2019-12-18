bec931dbcf914f86f65506b7abd3ea96602a1c94
Corentin Faucher <>
2019-12-18 13:29:39 -0500

    Chapitre 11, fini.

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 
 theapp = Flask(__name__)
 theapp.config.from_object(Config)
+bootstrap = Bootstrap(theapp)
 db = SQLAlchemy(theapp)
 migrate = Migrate(theapp, db)
 login = LoginManager(theapp)

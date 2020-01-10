ac829b06bd16b838b1ddbc4df092f135246887b2
Corentin Faucher <>
2020-01-10 10:25:31 -0500

    Chapitre 19, Envoie vers heroku.

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ class Config(object):
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
 	'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 	POSTS_PER_PAGE = 10
 	MAIL_SERVER = os.environ.get('MAIL_SERVER')
 	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
